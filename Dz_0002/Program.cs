4271b87059ff3df1f482ad095050178e654d94c4
Konstantin <kostya13111997@rambler.ru>
2023-12-17 14:15:21 +0300

    Initial commit

---
+++ Dz_0002/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[3];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+    System.Console.Write(array[i] + " ");
+}
+
+System.Console.WriteLine(count + " четных чисел");
+  
---
+++ Dz_0003/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[10];
+int[] newArray = new int[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    System.Console.Write(array[i] + " ");
+}
+System.Console.WriteLine();
+
+for (int i = array.Length - 1; i >= 0; i--)
+{
+    newArray[i] = array[i];
+    System.Console.Write(array[i] + " ");
+}
